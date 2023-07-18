b8fce8bd77b71b6a1f2ec551517f979112c27c4d
Mitra Patel <85149956+mitrapatel1909@users.noreply.github.com>
2023-07-18 16:31:46 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,14 @@
+var a=Number (prompt("Enter the first number"))
+var b=Number (prompt("Enter the first number"))
+function Addition(){
+    document.getElementById("add").innerHTML="The addition of these two numbers are "+(a+b)
+}
+function Subtraction(){
+    document.getElementById("sub").innerHTML="The subtraction of these two numbers are "+(a-b)
+}
+function Division(){
+    document.getElementById("divide").innerHTML="The division of these two numbers are "+(a/b)
+}
+function Multiplication(){
+    document.getElementById("multiply").innerHTML="The multilication of these two numbers are "+(a*b)
+}
